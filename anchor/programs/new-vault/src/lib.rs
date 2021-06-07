5a71c335c4178f6108f26009aa73c49d0960cb39
Theme <5batman400@gmail.com>
2021-06-08 01:53:09 +0700

    finish deposit

--- anchor/programs/new-vault/src/lib.rs
+++ anchor/programs/new-vault/src/lib.rs
@@ -3,7 +3,13 @@ use anchor_lang::prelude::*;
 use anchor_lang::solana_program::instruction::Instruction;
 use anchor_lang::solana_program::program::{invoke, invoke_signed};
 use anchor_lang::solana_program::program_option::COption;
+use anchor_lang::solana_program::{
+    program_error::ProgramError,
+    program_pack::{IsInitialized, Pack, Sealed},
+    pubkey::Pubkey,
+};
 use anchor_spl::token::{self, Burn, Mint, MintTo, TokenAccount, Transfer};
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 
 #[program]
 pub mod new_vault {
@@ -79,12 +85,14 @@ pub mod new_vault {
         token::transfer(cpi_ctx, amount)?;
 
         // calculate mint amount
-        // let current_deposit = vault_account.vault_user_info_account.
-        // let mint_amount = if ctx.accounts.vault_lp_token_account.amount == 0 {
-        //     amount
-        // } else {
-        //     amount * ctx.accounts.vault_token_mint_address.supply / ctx.accounts.vault_lp_token_account.amount
-        // };
+        let user_info = RaydiumUserInfoAccount::unpack_from_slice(
+            &ctx.accounts.vault_user_info_account.data.borrow(),
+        )?;
+        let mint_amount = if user_info.deposit_balance == 0 {
+            amount
+        } else {
+            amount * ctx.accounts.vault_token_mint_address.supply / user_info.deposit_balance
+        };
 
         // mint vault tokens to user's vault account
         msg!("mint vault token to user");
@@ -100,8 +108,7 @@ pub mod new_vault {
         };
         let cpi_program = ctx.accounts.token_program.clone();
         let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
-        // change mint amount duay
-        token::mint_to(cpi_ctx, amount)?;
+        token::mint_to(cpi_ctx, mint_amount)?;
 
         // deposit raydium
         msg!("deposit raydium");
@@ -334,7 +341,16 @@ pub struct WithdrawData {
     pub amount: u64,
 }
 
-#[account]
+// #[account]
+// pub struct RaydiumUserInfoAccount {
+//     pub state: u64,
+//     pub pool_id: Pubkey,
+//     pub staker_owner: Pubkey,
+//     pub deposit_balance: u64,
+//     pub reward_debt: u64,
+//     pub reward_debt_b: u64,
+// }
+
 pub struct RaydiumUserInfoAccount {
     pub state: u64,
     pub pool_id: Pubkey,
@@ -342,4 +358,63 @@ pub struct RaydiumUserInfoAccount {
     pub deposit_balance: u64,
     pub reward_debt: u64,
     pub reward_debt_b: u64,
+}
+
+impl Sealed for RaydiumUserInfoAccount {}
+
+impl IsInitialized for RaydiumUserInfoAccount {
+    fn is_initialized(&self) -> bool {
+        true
+    }
+}
+
+impl Pack for RaydiumUserInfoAccount {
+    const LEN: usize = 96;
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        let src = array_ref![src, 0, RaydiumUserInfoAccount::LEN];
+        let (state, pool_id, staker_owner, deposit_balance, reward_debt, reward_debt_b) =
+            array_refs![src, 8, 32, 32, 8, 8, 8];
+        // let is_initialized = match is_initialized {
+        //     [0] => false,
+        //     [1] => true,
+        //     _ => return Err(ProgramError::InvalidAccountData),
+        // };
+
+        Ok(RaydiumUserInfoAccount {
+            state: u64::from_le_bytes(*state),
+            pool_id: Pubkey::new_from_array(*pool_id),
+            staker_owner: Pubkey::new_from_array(*staker_owner),
+            deposit_balance: u64::from_le_bytes(*deposit_balance),
+            reward_debt: u64::from_le_bytes(*reward_debt),
+            reward_debt_b: u64::from_le_bytes(*reward_debt_b),
+        })
+    }
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        let dst = array_mut_ref![dst, 0, RaydiumUserInfoAccount::LEN];
+        let (
+            state_dst,
+            pool_id_dst,
+            staker_owner_dst,
+            deposit_balance_dst,
+            reward_debt_dst,
+            reward_debt_b_dst,
+        ) = mut_array_refs![dst, 8, 32, 32, 8, 8, 8];
+
+        let RaydiumUserInfoAccount {
+            state,
+            pool_id,
+            staker_owner,
+            deposit_balance,
+            reward_debt,
+            reward_debt_b,
+        } = self;
+
+        *state_dst = state.to_le_bytes();
+        pool_id_dst.copy_from_slice(pool_id.as_ref());
+        staker_owner_dst.copy_from_slice(staker_owner.as_ref());
+        *deposit_balance_dst = deposit_balance.to_le_bytes();
+        *reward_debt_dst = reward_debt.to_le_bytes();
+        *reward_debt_b_dst = reward_debt_b.to_le_bytes();
+    }
 }
